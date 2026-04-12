24329916e62916ee445887985653e7333f811b48
W3110y <wjiaohu005@gmail.com>
2026-04-12 12:57:36 +0200

    arreglar variables

--- teleteatros/teatros/Ticket.java
+++ teleteatros/teatros/Ticket.java
@@ -3,13 +3,9 @@ package teleteatros.teatros;
 public class Ticket {
 
     private int estado;
-    private Espectaculo espectaculo;
-    private Butaca butaca;
 
     public Ticket(int estado, Butaca butaca, Espectaculo espectaculo) {
         this.estado = estado;
-        this.butaca = butaca;
-        this.espectaculo = espectaculo;
     }
 
     public int getEstado() {
